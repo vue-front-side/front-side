74b76b061b3524786c7d685976502f7b53a74a6b
lovelybenny <923116169@qq.com>
2019-12-03 16:13:11 +0800

    加了更改密码，将所有信息判断写了

--- src/views/ChangeNumber.vue
+++ src/views/ChangeNumber.vue
@@ -1,111 +1,212 @@
 <template>
   <div class="disappointing">
-     <!-- 导航条 -->
-    <van-nav-bar title="更换手机号码" left-arrow @click-left="onClickLeft"/>
+    <!-- 导航条 -->
+    <van-nav-bar title="更换手机号码" left-arrow @click-left="onClickLeft" />
     <div class="phone-number">
       <h3>当前绑定手机号:</h3>
       <p class="recent-phonenumber">182******42</p>
     </div>
-    <form action="">
-      <div class="input-box">
-        <van-cell-group>
-          <van-field
-            readonly
-            clickable
-            label="手机号"
-            left-icon="phone-o"
-            placeholder="请输入新手机号"
-            :value="value" 
-            @touchstart.native.stop="show = true"
-          /> 
-          <van-number-keyboard
-            v-model="value"
-            :show="show"
-            :maxlength="11"
-            @blur="show = false"  
-
-          /> 
-        </van-cell-group>
-        <van-cell-group class="test-code">
-          <van-field
-            v-model="sms"
-            center
-            label="验证码"
-            label-width='2rem'
-            placeholder="请输入短信验证码"
-          >
-            <van-button slot="button" size="small" type="primary" color="#999">发送验证码</van-button>
-          </van-field>
-        </van-cell-group>
-      </div>
-      <div class="btn-box">
-        <button type="submit" class="confrim">确认</button>
-      </div>
-    </form>
-    
-  
+    <div class="input-box">
+      <van-cell-group>
+        <van-field
+          label="手机号"
+          placeholder="请输入手机号"
+          maxlength="11"
+          v-model="phonenumber"
+          @blur="phoneCheckOut"
+        />
+        <van-field label="姓名" placeholder="请输入您的姓名" v-model="name" @blur="nameCheckOut" />
+        <van-field
+          label="身份证号"
+          placeholder="请输入您的身份证号"
+          maxlength="18"
+          v-model="identity"
+          @blur="identityCheckOut"
+        />
+      </van-cell-group>
+      <van-cell-group class="test-code">
+        <van-field v-model="sms" label="验证码" placeholder="请输入短信验证码">
+          <van-button
+            slot="button"
+            size="small"
+            type="primary"
+            color="#999"
+            @click="sendSmsCode"
+          >发送验证码</van-button>
+        </van-field>
+      </van-cell-group>
+    </div>
+    <div class="btn-box">
+      <button class="confrim" @click="setPassSubmit">确认</button>
+    </div>
   </div>
 </template>
 
 
 <script>
+import { Toast } from "vant";
 export default {
-  name: 'changenumber',
   data() {
     return {
-      show: false,
-      value: '',
-      sms: ''
-    }
+      active: 0,
+      phonenumber: "",
+      name: "",
+      identity: "",
+      sms: "",
+      username: ""
+    };
+  },
+  components: {
+    [Toast.name]: Toast
   },
   methods: {
     onClickLeft() {
-      alert('返回');
+      this.$router.replace("/login");
+    },
+    phoneCheckOut() {
+      if (this.phonenumber != "") {
+        //当input 里面有数据的时候，再判断手机号对不对
+        var myreg = /^[1][3,4,5,7,8][0-9]{9}$/;
+        if (!myreg.test(this.phonenumber)) {
+          Toast("请填写正确手机号码！");
+          console.log(this.phonenumber);
+          this.phonenumber = "";
+        } else {
+          console.log("Yeah you got your correct number!");
+        }
+      }
+    },
+    nameCheckOut() {
+      if (this.name != "") {
+        //当input 里面有数据的时候，再判断手机号对不对
+        var myreg = /^[\u4e00-\u9fa5]{2,4}$/i;
+        if (!myreg.test(this.name)) {
+          Toast("请填写正确姓名！");
+          console.log(this.name);
+          this.name = "";
+        } else {
+          console.log("Yeah you got your correct name!");
+        }
+      }
+    },
+    identityCheckOut() {
+      if (this.identity != "") {
+        //当input 里面有数据的时候，再判断手机号对不对
+        var myreg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
+        if (!myreg.test(this.identity)) {
+          Toast("请填写正确身份证号！");
+          console.log(this.identity);
+          this.identity = "";
+        } else {
+          console.log("Yeah you got your correct identity!");
+        }
+      }
+    },
+    sendSmsCode() {
+      console.log(this.phonenumber);
+      if (this.phonenumber == "") {
+        Toast("请输入手机号！");
+      } else {
+        this.axios
+          .post("/user/modifyTelNumSms", {
+            telNum: this.phonenumber
+          })
+          .then(res => {
+            console.log(res.data);
+            var code = res.data.code;
+            if (code == 500) {
+              Toast("该手机号暂未注册哦！");
+            } else {
+              Toast("验证码发送成功！");
+            }
+          });
+      }
+    },
+    setPassSubmit() {
+      console.log(this.password);
+      console.log("注册");
+      console.log(this.sms);
+      if (this.phonenumber == "") {
+        Toast("请输入手机号！");
+      } else if (this.name == "") {
+        Toast("请输入姓名！");
+      } else if (this.identity == "") {
+        Toast("请输入身份证号！");
+      } else if (this.phonenumber && this.sms == "") {
+        Toast("请输入验证码！");
+      } else if (this.password == "" || this.passSure == "") {
+        Toast("请输入密码！");
+      } else {
+        this.axios
+          .post(
+            "/user/forgetPasswordSubmit",
+            {
+              telNum: this.phonenumber,
+              smsCode: this.sms,
+              password: this.password
+            },
+            {
+              headers: {
+                "content-type": "application/json",
+                validateId: this.validateId
+              }
+            }
+          )
+          .then(res => {
+            console.log(res);
+            if (res.data.code == "modify_success") {
+              // var token = "njaksxbxkjasbkjcxasbjk" // 模拟后台返回的token
+              var token = res.data.data.token;
+              var userId = res.data.data.user.userId;
+              var userState = res.data.data.user.userId;
+              sessionStorage.setItem("token", token);
+              sessionStorage.setItem("userId", userId);
+              sessionStorage.setItem("userId", userState);
+              console.log(userId);
+              Toast("密码修改成功!");
+              // 获取参数（未登录时想访问的路由）
+              var url = this.$route.query.redirect;
+
+              url = url ? url : "/index";
+              // 切换路由
+              this.$router.replace(url);
+              // this.axios.post("/test")
+            } else {
+              Toast("密码修改失败！");
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
     }
   }
-}
+};
 </script>
 
 
 <style lang="less" scoped>
-@import '../assets/style/base.less';
-@import '../assets/less/nav.less';
-h3{
+@import "../assets/style/base.less";
+@import "../assets/less/nav.less";
+h3 {
   font-weight: 500;
 }
 .disappointing {
   background-color: rgb(248, 248, 248);
-  height: 1196px;
+  height: 647px;
 }
 .phone-number {
-  padding: 20px 10px 10px; 
+  padding: 20px 10px 10px;
 }
 .recent-phonenumber {
   text-align: center;
   margin-top: 20px;
 }
-.input-box{
+.input-box {
   width: 100%;
   margin-top: 20px;
 }
-// .van-cell--clickable {
-//   font-size: 18px;
-//   color: #666;
-// }
-// .van-icon-phone-o{
-//   font-size: 20px;
-// }
-// /* .van-field__control {
-//   padding-left: 20px;
-//   box-sizing: border-box;
-// } */
-// .van-button--small {
-//   height: 80px;
-//   width: 160px;
-//   margin-top: 0px;
-//   transform: translateY(-3px);
-// }
-.btn-box{
+.btn-box {
   text-align: center;
   margin-top: 35px;
 }
@@ -118,5 +219,4 @@ h3{
   font-size: 20px;
   color: white;
 }
-
 </style>
--- src/views/ForgetPass.vue
+++ src/views/ForgetPass.vue
@@ -1,75 +1,163 @@
 <template>
   <div>
     <van-nav-bar title="忘记密码" left-arrow @click-left="onClickLeft"/>
-    <form action="" class="form-area">
         <div class="input-box">
           <van-cell-group>
             <van-field
-              readonly
-              clickable
               label="手机号"
               placeholder="请输入手机号"
-              :value="phonenumber" 
-              @touchstart.native.stop="show = true"
-            /> 
-            <van-number-keyboard
+              maxlength="11"
               v-model="phonenumber"
-              :show="show"
-              :maxlength="11"
-              @blur="show = false"  
-            /> 
+              @blur="phoneCheckOut"
+            />
           </van-cell-group>
           <van-cell-group class="test-code">
             <van-field
               v-model="sms"
-              center
               label="验证码"
-              label-width='2rem'
               placeholder="请输入短信验证码"
             >
-              <van-button slot="button" size="small" type="primary" color="#999">发送验证码</van-button>
+              <van-button slot="button" size="small" type="primary" color="#999" @click="sendSmsCode">发送验证码</van-button>
             </van-field>
           </van-cell-group>
           <van-cell-group class="pass-box">
             <van-field
-              readonly
-              clickable
               label="密码"
+              type="password"
               placeholder="请输入密码"
-              :value="password" 
+              v-model="password" 
             /> 
             <van-field
-              readonly
-              clickable
               label="新密码"
+              type="password"
               placeholder="请输入新密码"
-              :value="newpassword" 
+              v-model="passSure" 
+              @blur="passCheckOut"
             /> 
           </van-cell-group>
         </div>
         <div class="btn-box">
-          <button type="submit" class="confrim">确认</button>
+          <button class="confrim" @click="setPassSubmit">确认</button>
         </div>
-      </form>
   </div>
 </template>
 
 <script>
+import { Toast } from "vant";
 export default {
   data() {
     return {
-      show: false,
       active: 0,
       phonenumber: '',
+      name: '',
+      identity: '',
       sms: '',
       username: '',
       password: '',
-      newpassword: ''
+      passSure: ''
     }
+  },
+   components: {
+    [Toast.name]: Toast
   },
   methods: {
     onClickLeft() {
-      alert('返回');
+      this.$router.replace('/login')
+    },
+    phoneCheckOut() {
+      if (this.phonenumber != "") {
+        //当input 里面有数据的时候，再判断手机号对不对
+        var myreg = /^[1][3,4,5,7,8][0-9]{9}$/;
+        if (!myreg.test(this.phonenumber)) {
+          Toast("请填写正确手机号码！");
+          console.log(this.phonenumber);
+          this.phonenumber = "";
+        } else {
+          console.log("Yeah you got your correct number!");
+        }
+      }
+    },
+    passCheckOut() {
+      if (this.passSure || this.password) {
+        if (this.password != this.passSure) {
+          Toast("两次密码输入不一致！");
+          this.passSure = '';
+        } else if (this.passSure == "") {
+          Toast("请再次输入密码！");
+        }
+      }
+    },
+    sendSmsCode() {
+      console.log(this.phonenumber);
+      if (this.phonenumber == "") {
+        Toast("请输入手机号！");
+      } else {
+        this.axios
+        .post("/user/loginSendSms", {
+          telNum: this.phonenumber
+        })
+        .then(res => {
+          console.log(res.data);
+          var code = res.data.code;
+          if (code == 500) {
+            Toast("该手机号暂未注册哦！");
+          } else {
+            Toast("验证码发送成功！");
+          }
+        });
+      }
+    },
+    setPassSubmit()  {
+      console.log(this.password);
+      console.log("注册");
+      console.log(this.sms);
+      if (this.phonenumber == "") {
+        Toast("请输入手机号！");
+      } else if (this.phonenumber && this.sms == "") {
+        Toast("请输入验证码！");
+      } else if (this.password == "" || this.passSure == '') {
+        Toast("请输入密码！");
+      } else {
+        this.axios
+        .post("/user/forgetPasswordSubmit", {
+            telNum: this.phonenumber,
+            smsCode: this.sms,
+            password: this.password
+          },
+          {
+            headers: {
+              "content-type": "application/json",
+              validateId: this.validateId
+            }
+          }
+        )
+        .then(res => {
+          console.log(res);
+          if (res.data.code == "modify_success") {
+            // var token = "njaksxbxkjasbkjcxasbjk" // 模拟后台返回的token
+            var token = res.data.data.token;
+            var userId = res.data.data.user.userId;
+            var userState = res.data.data.user.userId;
+            sessionStorage.setItem("token", token);
+            sessionStorage.setItem("userId", userId);
+            sessionStorage.setItem("userId", userState);
+            console.log(userId);
+            Toast("密码修改成功!");
+            // 获取参数（未登录时想访问的路由）
+            var url = this.$route.query.redirect;
+
+            url = url ? url : "/index";
+            // 切换路由
+            this.$router.replace(url);
+            // this.axios.post("/test")
+          } else {
+            Toast("密码修改失败！")
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      }
     }
   }
 }
@@ -78,7 +166,7 @@ export default {
 <style lang="less" scoped>
 @import '../assets/style/base.less';
 @import '../assets/less/nav.less';
-.form-area {
+.input-box {
   margin-top: 25px;
 }
 .pass-box {
